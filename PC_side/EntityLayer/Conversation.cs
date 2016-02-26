091ad3b66bb9256b950e73f05d47630362aade5e
Kami <lekamikaze03@hotmail.fr>
2016-02-26 14:16:10 +0100

    Ajout des entités métier.

---
+++ PC_side/EntityLayer/Conversation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Conversation
+    {
+
+        private Contact receiver;
+        private List<SMS> messages;
+
+        public Conversation()
+        {
+
+        }
+    }
+}
--- PC_side/Interface/UserControls/BulleConvSender.xaml.cs
+++ PC_side/Interface/UserControls/BulleConvSender.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,28 @@ namespace Projet.UserControls
     /// </summary>
     public partial class BulleConvSender : UserControl
     {
+        public DateTime Date
+        {
+            get { return DateTime.Parse(this.senderDate.Text); }
+            set { this.senderDate.Text = value.ToString("HH:mm",CultureInfo.InvariantCulture); }
+        }
+
+        public String SenderName
+        {
+            get { return this.senderName.Text; }
+            set { this.senderName.Text = value; }
+        }
+
+        public String Message
+        {
+            get { return this.senderMessage.Text; }
+            set { this.senderMessage.Text = value; }
+        }
+
         public BulleConvSender()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
     }
 }
